c8d561e8e993600233b51fe258b709d65213c4aa
Daniel Evans <danharibo@gmail.com>
2014-01-28 20:42:07 +0000

    More test

--- source/game/GameBoard.hpp
+++ source/game/GameBoard.hpp
@@ -2,9 +2,56 @@
 #ifndef _GAME_BOARD_HPP
 #define _GAME_BOARD_HPP
 
+/**
+ * Rough notes on grid layout
+ * Game board is roughly octagonal
+ *
+ * 11 Rows of alternating length (6 : 5)
+ * Token states := 
+ *   Horizontal (Connecting within row)
+ *   Vertical (Connecting adjacent rows) 
+ * Token Orientation :=
+ *   Row Colour => Horizontal
+ *   !Row Colour => Vertical
+ */
+
+enum TokenOrientation {
+	O_VERTICAL,
+	O_HORIZONTAL
+};
+enum TokenColour {
+	T_EMPTY,
+	T_RED,
+	T_WHITE
+};
+
+class BoardRow
+{
+	std::vector<ToeknColour> tokens;
+	TokenColour rowColour;
+public:
+	BoardRow(TokenColour colour, size_t length);
+
+	/**
+	 * Put a token into this row
+	 */
+	void putToken(size_t i, TokenColour colour);
+
+	/**
+	 * Returns the token in the row at the given index
+	 */
+	TokenColour getColour(size_t i) const;
+
+	/**
+	 * Returns the orientation in the row at the given index
+	 */
+	TokenOrientation getOrientation(size_t i) const;
+};
+
 class GameBoard
 {
-
+public:
+	GameBoard(size_t length);
 };
 
 #endif
---
+++ tests/test_GameBoard.cpp
@@ -0,0 +1,37 @@
+#include <GameBoard.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(GameBoardTests)
+
+BOOST_AUTO_TEST_CASE(Row_Test)
+{
+	BoardRow row(T_RED, 5);
+
+	// Insert some tokens on this row
+	row.putToken(1, T_RED);
+	row.putToken(2, T_RED);
+	row.putToken(3, T_WHITE);
+
+	BOOST_CHECK_EQUAL( row.getColour(1), T_RED);
+	BOOST_CHECK_EQUAL( row.getColour(3), T_WHITE);
+
+	// Validate orientation
+	BOOST_CHECK_EQUAL( row.getOrientation(1), O_HORIZONTAL);
+	BOOST_CHECK_EQUAL( row.getOrientation(3), O_VERTICAL);
+}
+
+BOOST_AUTO_TEST_CASE(Board_Test)
+{
+	GameBoard board(5);	
+
+	board.putToken(1, 2, T_RED);
+	board.putToken(2, 2, T_WHITE);
+
+	BOOST_CHECK_EQUAL( board.getColour(1, 2), T_RED);
+	BOOST_CHECK_EQUAL( board.getColour(2, 2), T_WHITE);
+
+	BOOST_CHECK_EQUAL( board.getOrientation(1, 2), O_HORIZONTAL);
+	BOOST_CHECK_EQUAL( board.getOrientation(2, 2), O_VERTICAL);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
